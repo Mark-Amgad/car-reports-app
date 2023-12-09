cd236eb01bee87a2c8afecf55394cda660fb4c9e
Mark-Amgad <markamgad5@gmail.com>
2023-12-09 16:59:32 +0200

    feat(auth): unit testing for login

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -40,52 +40,113 @@ describe('AuthService : ', () => {
     expect(service).toBeDefined();
   });
 
-  it('SignUp: should return the same Email in the input', async () => {
-    const email = 'mark@gmail.com';
-    const user = await service.signUp('Mark', email, '1234');
+  describe('Sign-up : ', () => {
+    it('SignUp: should return the same Email in the input', async () => {
+      const email = 'mark@gmail.com';
+      const user = await service.signUp('Mark', email, '1234');
 
-    expect(user.email).toEqual(email);
+      expect(user.email).toEqual(email);
+    });
+
+    it('SignUp: should return the same Name in the input', async () => {
+      const name = 'mark';
+      const user = await service.signUp('mark', 'any@gmail.com', '1234');
+
+      expect(user.name).toEqual(name);
+    });
+
+    it('SignUp: should return differenct inputh than in the input', async () => {
+      const pass = '1234';
+      const user = await service.signUp('mark', 'any@gmail.com', pass);
+
+      expect(user.password).not.toEqual(pass);
+    });
+
+    it('SignUp: the password should be hashed', async () => {
+      const pass = '1234';
+      const user = await service.signUp('mark', 'any@gmail.com', pass);
+
+      const resultedPass = user.password;
+      const [salt, hashedPass] = resultedPass.split('.');
+
+      expect(salt).toBeDefined();
+      expect(hashedPass).toBeDefined();
+    });
+
+    it('SignUp: Should throw error if the email is already exist', async () => {
+      fakeUsersService.getByEmail = (email: string) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            name: 'mark',
+            email: 'mark@gmail.com',
+            password: '1234',
+          } as User,
+        ]);
+      };
+
+      await expect(
+        service.signUp('mark', 'any@gmail.com', '1234'),
+      ).rejects.toThrow(BadRequestException);
+    });
   });
 
-  it('SignUp: should return the same Name in the input', async () => {
-    const name = 'mark';
-    const user = await service.signUp('mark', 'any@gmail.com', '1234');
+  describe('login : ', () => {
+    it('Login: should throw an error if the email does not exist', async () => {
+      await expect(service.login('mark@gmail.com', '1234')).rejects.toThrow(
+        BadRequestException,
+      );
+    });
 
-    expect(user.name).toEqual(name);
-  });
+    it('Login: should throw an error if the password is wrong', async () => {
+      fakeUsersService.getByEmail = (email: string) => {
+        return Promise.resolve([
+          {
+            id: 1,
+            email: 'mark@gmail.com',
+            name: 'mark',
+            password: '1234',
+          } as User,
+        ]);
+      };
 
-  it('SignUp: should return differenct inputh than in the input', async () => {
-    const pass = '1234';
-    const user = await service.signUp('mark', 'any@gmail.com', pass);
+      await expect(service.login('mark@gmail.com', '567')).rejects.toThrow(
+        BadRequestException,
+      );
+    });
 
-    expect(user.password).not.toEqual(pass);
-  });
-
-  it('SignUp: the password should be hashed', async () => {
-    const pass = '1234';
-    const user = await service.signUp('mark', 'any@gmail.com', pass);
-
-    const resultedPass = user.password;
-    const [salt, hashedPass] = resultedPass.split('.');
-
-    expect(salt).toBeDefined();
-    expect(hashedPass).toBeDefined();
-  });
-
-  it('SignUp: Should throw error if the email is already exist', async () => {
-    fakeUsersService.getByEmail = (email: string) => {
-      return Promise.resolve([
-        {
+    it('Login: should return a user if both credentials are correct', async () => {
+      const users: User[] = [];
+      fakeUsersService.create = (
+        name: string,
+        email: string,
+        password: string,
+      ) => {
+        const user: User = {
           id: 1,
-          name: 'mark',
-          email: 'mark@gmail.com',
-          password: '1234',
-        } as User,
-      ]);
-    };
+          name: name,
+          email: email,
+          password: password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      };
 
-    await expect(
-      service.signUp('mark', 'any@gmail.com', '1234'),
-    ).rejects.toThrow(BadRequestException);
+      fakeUsersService.getByEmail = (email: string) => {
+        const user = users.filter((user) => {
+          return user.email === email;
+        });
+        return Promise.resolve(user);
+      };
+
+      const name = 'mark';
+      const email = 'mark@gmail.com';
+      const pass = '1234';
+      await service.signUp(name, email, pass);
+
+      const user = await service.login(email, pass);
+
+      expect(user).toBeDefined();
+    });
   });
 });
